1134ddbea13e2ed666ab08fa82e70c7c31c155c9
Yuria Loo <129998654+yuria-loo@users.noreply.github.com>
2024-05-30 21:08:39 -0700

    Add files via upload

---
+++ week8/env/dbase.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Create a Flask application instance
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
